23d9e157eac817ef5daacba6adfc1bffe8ecf407
gwest <gwest@redhat.com>
2019-03-18 12:55:28 -0500

    Fixup kubernetes pull

--- getkube.sh
+++ getkube.sh
@@ -5,7 +5,7 @@ mkdir wip
 mkdir wip/bin
 export VERSION=v1.11.3
 curl -L -k https://dl.k8s.io/$VERSION/kubernetes-node-windows-amd64.tar.gz -o /tmp/kubernetes.tar.gz
-tar xvzf /tmp/kubernetes.tar.gz src
+(cd src;tar xvzf /tmp/kubernetes.tar.gz )
 cp src/kubernetes/node/bin/* wip/bin
 
 
