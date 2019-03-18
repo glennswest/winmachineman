9134da09791521fab8a9930d0e7462007c0f197d
gwest <gwest@redhat.com>
2019-03-18 12:49:04 -0500

    Initial Commit

---
+++ getkube.sh
@@ -0,0 +1,11 @@
+rm -r -f wip
+rm -r -f src
+mkdir src
+mkdir wip
+mkdir wip/bin
+export VERSION=v1.11.3
+curl -L -k https://dl.k8s.io/$VERSION/kubernetes-node-windows-amd64.tar.gz -o /tmp/kubernetes.tar.gz
+tar xvzf /tmp/kubernetes.tar.gz src
+cp src/kubernetes/node/bin/* wip/bin
+
+
