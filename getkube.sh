1ae4b7d8df62676ec58a9844ef63ba535229dcd7
gwest <gwest@redhat.com>
2019-03-19 09:02:58 -0500

    Validated for kube conetnt build

--- getkube.sh
+++ getkube.sh
@@ -7,5 +7,7 @@ export VERSION=v1.11.3
 curl -L -k https://dl.k8s.io/$VERSION/kubernetes-node-windows-amd64.tar.gz -o /tmp/kubernetes.tar.gz
 (cd src;tar xvzf /tmp/kubernetes.tar.gz )
 cp src/kubernetes/node/bin/* wip/bin
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/kube_$VERSION.ign bin)
+$GOPATH/src/github.com/glennswest/libignition/igntool/igntool ls content/kube_$VERSION.ign
 
 
