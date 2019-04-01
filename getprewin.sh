522c34159dba1e37a66d2329aa2213b18be42574
gwest <gwest@redhat.com>
2019-03-31 20:52:27 -0500

    Fix up prereq

--- getprewin.sh
+++ getprewin.sh
@@ -3,13 +3,14 @@ mkdir wip
 mkdir wip/bin
 mkdir wip/bin/metadata
 export VERSION=v1.0
-cp metadata/prewin1809_$VERION.metadata wip/bin/metadata
-cp files/prewin1809_$VERSION/bin/* wip/bin
+rm content/prewin1809_$VERSION.ign
+cp metadata/prewin1809_$VERSION.metadata wip/bin/metadata
+cp files/prewin1809_$VERSION/bin/*.ps wip/bin
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/prewin1809_$VERSION.ign bin)
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ../content/prewin1809_$VERSION.ign bin/metadata/prewin1809_$VERSION.metadata)
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/prewin1809_$VERSION.ign bin/metadata)
 $GOPATH/src/github.com/glennswest/libignition/igntool/igntool ls content/prewin1809_$VERSION.ign
 cat wip/bin/metadata/prewin1809_$VERSION.metadata
-rm -r -f wip/*
+#rm -r -f wip/*
 
 
