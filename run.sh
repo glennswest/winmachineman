2ec1af38b533cd4f3683fd9fd91bfbccc20fa76a
glennswest <gwest@redhat.com>
2019-04-02 13:18:12 +0800

    Add run command

---
+++ run.sh
@@ -0,0 +1,17 @@
+git pull
+export GIT_COMMIT=$(git rev-parse --short HEAD)
+echo $GIT_COMMIT
+export pname=winmachineman
+oc delete dc $pname
+oc delete is $pname
+oc delete sa $pname
+oc delete project $pname
+sleep 3
+oc new-project $pname
+oc import-image $pname --from=docker.io/glennswest/$pname:$GIT_COMMIT --confirm
+#oc create sa $pname
+#oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:$pname:default
+#oc policy add-role-to-user admin  system:serviceaccount:$pname:default
+oc delete  istag/$pname:latest
+oc new-app glennswest/$pname:$GIT_COMMIT --token=$(oc sa get-token $pname) 
+
