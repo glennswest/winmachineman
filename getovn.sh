d0f324df4c83f37f34684372cc6ec7ed8a730e55
glennswest <gwest@redhat.com>
2019-05-09 09:56:01 -0500

    getovn - fix version tag and make sure metadata is included

--- getovn.sh
+++ getovn.sh
@@ -6,10 +6,11 @@ cd wip/ovn-kubernetes;export VERSION=`git rev-parse --short HEAD`;cd ../..
 (cd wip/ovn-kubernetes;cd go-controller;make clean;make windows)
 cp wip/ovn-kubernetes/go-controller/_output/go/windows/*.exe wip/bin
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/ovn_$VERSION.ign bin)
+cp metadata/ovn_$VERSION.metadata wip/bin/metadata/ovn_$VERSION.metadata
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ../content/ovn_$VERSION.ign bin/metadata/ovn_$VERSION.metadata)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/ovn_$VERSION.ign bin/metadata)
 $GOPATH/src/github.com/glennswest/libignition/igntool/igntool ls content/ovn_$VERSION.ign
 pwd
 rm -r -f wip/bin
 rm -r -f wip/ovn-kubernetes
-
-
 
