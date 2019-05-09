4699b058681ffd6def51befddc233c528c77f844
glennswest <gwest@redhat.com>
2019-05-09 09:59:58 -0500

    Create missing metadata folder in wip for ovn module

--- getovn.sh
+++ getovn.sh
@@ -1,6 +1,7 @@
 rm -r -f wip/ovn-kubernetes
 rm -r -f wip/bin
 mkdir wip/bin
+mkdir wip/bin/metadata
 git clone https://github.com/glennswest/ovn-kubernetes  wip/ovn-kubernetes
 cd wip/ovn-kubernetes;export VERSION=`git rev-parse --short HEAD`;cd ../..
 (cd wip/ovn-kubernetes;cd go-controller;make clean;make windows)
@@ -11,6 +12,6 @@ cp metadata/ovn_$VERSION.metadata wip/bin/metadata/ovn_$VERSION.metadata
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/ovn_$VERSION.ign bin/metadata)
 $GOPATH/src/github.com/glennswest/libignition/igntool/igntool ls content/ovn_$VERSION.ign
 pwd
-rm -r -f wip/bin
-rm -r -f wip/ovn-kubernetes
+#rm -r -f wip/bin
+#rm -r -f wip/ovn-kubernetes
 
