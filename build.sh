5d2695d45c1f9c1f317d87506978ca3aab56bfe7
glennswest <gwest@redhat.com>
2019-05-10 16:03:30 -0500

    fix build to track md5 of content

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@ cp ../winnodemanager/winnodeman.version content
 git add -f content/winnodeman.version
 git commit -a -m "winnodeman version change - rebuild container"
 git push origin master
+md5sum content/* > content/ignition.md5
+git add content/ignition/md5
+git commit -a -m "content has changed - rebuild container"
+git push origin master
 export GIT_COMMIT=$(git rev-parse --short HEAD)
 rm -r -f tmp
 mkdir tmp
