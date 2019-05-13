26643f972e3f425b2ee666e3a3a76b04d6bafa8d
glennswest <gwest@redhat.com>
2019-05-13 15:20:25 -0500

    Component prewin_1809: Fix powershell script naming

--- getprewin.sh
+++ getprewin.sh
@@ -5,7 +5,7 @@ mkdir wip/bin/metadata
 export VERSION=v1.0
 rm content/prewin1809_$VERSION.ign
 cp metadata/prewin1809_$VERSION.metadata wip/bin/metadata
-cp files/prewin1809_$VERSION/bin/*.ps wip/bin
+cp files/prewin1809_$VERSION/bin/*.ps1 wip/bin
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/prewin1809_$VERSION.ign bin)
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ../content/prewin1809_$VERSION.ign bin/metadata/prewin1809_$VERSION.metadata)
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/prewin1809_$VERSION.ign bin/metadata)
