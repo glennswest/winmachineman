6cd465161fe99ead9767f7abc8348ad2b07a21d1
glennswest <gwest@redhat.com>
2019-05-17 14:33:20 -0500

    component docker: Fix metadata and fix script directory

--- getdocker.sh
+++ getdocker.sh
@@ -8,9 +8,9 @@ echo $VERSION
 echo $COMPNAME
 rm content/${COMPNAME}_${VERSION}.ign
 cp metadata/${COMPNAME}_${VERSION}.metadata wip/bin/metadata
-cp files/$COMPNAME_$VERSION/bin/*.ps1 wip/bin
+cp files/${COMPNAME}_${VERSION}/bin/*.ps1 wip/bin
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/${COMPNAME}_${VERSION}.ign bin)
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ../content/${COMPNAME}_${VERSION}.ign bin/metadata/$COMPNAME_$VERSION.metadata)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ../content/${COMPNAME}_${VERSION}.ign bin/metadata/${COMPNAME}_${VERSION}.metadata)
 (cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/${COMPNAME}_${VERSION}.ign bin/metadata)
 $GOPATH/src/github.com/glennswest/libignition/igntool/igntool ls content/${COMPNAME}_${VERSION}.ign
 cat wip/bin/metadata/${COMPNAME}_${VERSION}.metadata
