43fcf8513cd628af4f45a8065e9f5073bd2ef7c8
glennswest <gwest@redhat.com>
2019-05-19 16:28:29 -0500

    component docker: Remove add-content

--- files/docker_v1.0.1/bin/docker_v1.0.1.ps1
+++ files/docker_v1.0.1/bin/docker_v1.0.1.ps1
@@ -1,19 +1,11 @@
+Write-Output "Docker Install Starting"
 $ErrorActionPreference = "Stop"
 $DOCKER_SERVICE_NAME = "Docker"
-Write-Output "Find Package Provider"
-Add-Content c:\docker-install.out "Start Find Package Provider"
 Find-PackageProvider -Name "Nuget" | Install-PackageProvider -Force
-Add-Content c:\docker-install.out "Find Package Provider - Completed"
-Add-Content c:\docker-install.out "Start Install-Module DockerMsftProvider"
 Install-Module -Name "DockerMsftProvider" -Repository "PSGallery" -Force
-Add-Content c:\docker-install.out "Install-Module DockerMsftProvider - Completed"
-Add-Content c:\docker-install.out "Install-Package Docker - Starting"
 Install-Package -Name "Docker" -ProviderName "DockerMsftProvider" -Force 
-Add-Content docker-install.out "Install-Package Docker - Completed"
 Start-Sleep -s 30
-Add-Content c:\docker-install.out "Set-Service"
 Set-Service $DOCKER_SERVICE_NAME -StartupType Disabled
-Add-Content c:\docker-install.out "Stop-Service"
 Stop-Service $DOCKER_SERVICE_NAME
 Get-HnsNetwork | Where-Object { $_.Name -eq "nat" } | Remove-HnsNetwork
 $configFile = Join-Path $env:ProgramData "Docker\config\daemon.json"
@@ -22,7 +14,6 @@ if(!(Test-Path $configDir)) {
     New-Item -ItemType "Directory" -Force -Path $configDir
     }
 Set-Content -Path $configFile -Value '{ "bridge" : "none" }' -Encoding Ascii
-Add-Content c:\scripts\test.txt "Set-Service Auto Mode"
 Set-Service $DOCKER_SERVICE_NAME -StartupType Automatic
 sc.exe failure $DOCKER_SERVICE_NAME reset=40 actions=restart/0/restart/0/restart/30000
 if($LASTEXITCODE) {
@@ -32,7 +23,6 @@ sc.exe failureflag $DOCKER_SERVICE_NAME 1
 if($LASTEXITCODE) {
       Throw "Failed to set failure flags"
       }
-Add-Content c:\docker-install.out "Final Start Service"
 Start-Service $DOCKER_SERVICE_NAME
-Write-Output "Install Finished"
+Write-Output "Docker Install Finished"
 
