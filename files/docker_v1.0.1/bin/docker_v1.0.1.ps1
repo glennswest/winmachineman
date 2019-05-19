2f6a71e47bb21e3b4b89ecdb67f68dd2a5ad614f
glennswest <gwest@redhat.com>
2019-05-18 20:13:56 -0500

    Add tracking of docker install

--- files/docker_v1.0.1/bin/docker_v1.0.1.ps1
+++ files/docker_v1.0.1/bin/docker_v1.0.1.ps1
@@ -1,10 +1,19 @@
 $ErrorActionPreference = "Stop"
 $DOCKER_SERVICE_NAME = "Docker"
+Write-Output "Find Package Provider"
+Add-Content docker-install.out "Start Find Package Provider"
 Find-PackageProvider -Name "Nuget" | Install-PackageProvider -Force
+Add-Content docker-install.out "Find Package Provider - Completed"
+Add-Content docker-install.out "Start Install-Module DockerMsftProvider"
 Install-Module -Name "DockerMsftProvider" -Repository "PSGallery" -Force
+Add-Content docker-install.out "Install-Module DockerMsftProvider - Completed"
+Add-Content docker-install.out "Install-Package Docker - Starting"
 Install-Package -Name "Docker" -ProviderName "DockerMsftProvider" -Force 
+Add-Content docker-install.out "Install-Package Docker - Completed"
 Start-Sleep -s 30
+Add-Content docker-install.out "Set-Service"
 Set-Service $DOCKER_SERVICE_NAME -StartupType Disabled
+Add-Content docker-install.out "Stop-Service"
 Stop-Service $DOCKER_SERVICE_NAME
 Get-HnsNetwork | Where-Object { $_.Name -eq "nat" } | Remove-HnsNetwork
 $configFile = Join-Path $env:ProgramData "Docker\config\daemon.json"
@@ -13,6 +22,7 @@ if(!(Test-Path $configDir)) {
     New-Item -ItemType "Directory" -Force -Path $configDir
     }
 Set-Content -Path $configFile -Value '{ "bridge" : "none" }' -Encoding Ascii
+Add-Content c:\scripts\test.txt "Set-Service Auto Mode"
 Set-Service $DOCKER_SERVICE_NAME -StartupType Automatic
 sc.exe failure $DOCKER_SERVICE_NAME reset=40 actions=restart/0/restart/0/restart/30000
 if($LASTEXITCODE) {
@@ -22,4 +32,7 @@ sc.exe failureflag $DOCKER_SERVICE_NAME 1
 if($LASTEXITCODE) {
       Throw "Failed to set failure flags"
       }
+Add-Content c:\scripts\test.txt "Final Start Service"
 Start-Service $DOCKER_SERVICE_NAME
+Write-Output "Install Finished"
+
