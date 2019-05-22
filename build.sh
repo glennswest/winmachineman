84e18f5947a56c703efd8ce7386991ae53472660
glennswest <gwest@redhat.com>
2019-05-22 15:38:25 -0500

    Cleanup build

--- build.sh
+++ build.sh
@@ -1,18 +1,7 @@
-rm commit.id
-md5sum content/* > content/ignition.md5
-cp ../winnodemanager/winnodeman.version content
-git add -f content/winnodeman.version
-git add -f content/ignition.md5
-git commit -a -m "winnodeman version change - rebuild container"
 git push origin master
 export GIT_COMMIT=$(git rev-parse --short HEAD)
 rm -r -f tmp
 mkdir tmp
-echo $GIT_COMMIT > commit.id
-#eval $(minishift docker-env)
-rm content/winnodeman.exe
-cp ../winnodemanager/winnodeman.exe content
 docker build --no-cache -t glennswest/winmachineman:$GIT_COMMIT .
 docker tag glennswest/winmachineman:$GIT_COMMIT  docker.io/glennswest/winmachineman:$GIT_COMMIT
 docker push docker.io/glennswest/winmachineman:$GIT_COMMIT
-rm commit.id
