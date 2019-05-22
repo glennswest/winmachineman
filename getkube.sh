8773dbde9a3f26d015705ff5a3263a75e3dd8739
glennswest <gwest@redhat.com>
2019-05-22 15:36:51 -0500

    winnodeman version change - rebuild container

--- getkube.sh
+++ getkube.sh
@@ -8,11 +8,12 @@ export VERSION=v1.11.3
 curl -L -k https://dl.k8s.io/$VERSION/kubernetes-node-windows-amd64.tar.gz -o /tmp/kubernetes.tar.gz
 (cd src;tar xvzf /tmp/kubernetes.tar.gz )
 cp src/kubernetes/node/bin/* wip/bin
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/kube_$VERSION.ign bin)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${GOPATH}/src/github.com/wcontent/content/kube_$VERSION.ign bin)
 cp metadata/kube_$VERSION.metadata wip/bin/metadata/kube_$VERSION.metadata
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ../content/kube_$VERSION.ign bin/metadata/kube_$VERSION.metadata)
-(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ../content/kube_$VERSION.ign bin/metadata)
-$GOPATH/src/github.com/glennswest/libignition/igntool/igntool ls content/kube_$VERSION.ign
+rm $GOPATH/src/github.com/glennswest/wcontent/content/kube_$VERSION.ign
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool um ${GOPATH}/src/github.com/wcontent/content/kube_$VERSION.ign bin/metadata/kube_$VERSION.metadata)
+(cd wip;$GOPATH/src/github.com/glennswest/libignition/igntool/igntool a ${GOPATH}/src/github.com/wcontent/content/kube_$VERSION.ign bin/metadata)
+$GOPATH/src/github.com/glennswest/libignition/igntool/igntool ls ${GOPATH}/src/github.com/wcontent/content/kube_$VERSION.ign
 cat wip/bin/metadata/kube_$VERSION.metadata
 rm -r -f wip/*
 rm -r -f src
