44953c08d9c4b489b28e915c629de33bc99598e7
gwest <gwest@redhat.com>
2019-06-29 05:06:06 +0800

    Fixup run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ oc delete dc $pname
 oc delete is $pname
 oc delete sa $pname
 oc delete project $pname
-sleep 3
+sleep 20
 oc new-project $pname
 oc import-image $pname --from=docker.io/glennswest/$pname:$GIT_COMMIT --confirm
 #oc create sa $pname
@@ -14,7 +14,8 @@ oc import-image $pname --from=docker.io/glennswest/$pname:$GIT_COMMIT --confirm
 #oc policy add-role-to-user admin  system:serviceaccount:$pname:default
 oc delete  istag/$pname:latest
 oc new-app glennswest/$pname:$GIT_COMMIT --token=$(oc sa get-token $pname) 
-export defaultdomain=$(oc describe route docker-registry --namespace=default | grep "Requested Host" | cut -d ":" -f 2 | cut -d "." -f 2-)
+export defaultdomain=$(oc describe --namespace=openshift-ingress-operator ingresscontroller/default | grep "Domain:" | cut -d ":" -f 2 | cut -d "." -f 1-)
 oc expose svc/winmachineman --hostname=winmachineman.$defaultdomain
-#oc set env dc/winmachineman MYURL=winmachineman.$defaultdomain
-oc set env dc/winmachineman  MYURL=https://media.githubusercontent.com/media/glennswest/wcontent/master
+sleep 20
+#oc set env dc/winmachineman  MYURL=https://media.githubusercontent.com/media/glennswest/wcontent/master
+oc set env dc/winmachineman MYURL=winoperatordata.$defaultdomain
