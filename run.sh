5e62549837c9cdc9d0e5d2e25ce682568da7d04e
gwest <gwest@redhat.com>
2019-06-29 05:14:03 +0800

    Remove extra space in domain name in run.sh

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ oc import-image $pname --from=docker.io/glennswest/$pname:$GIT_COMMIT --confirm
 #oc policy add-role-to-user admin  system:serviceaccount:$pname:default
 oc delete  istag/$pname:latest
 oc new-app glennswest/$pname:$GIT_COMMIT --token=$(oc sa get-token $pname) 
-export defaultdomain=$(oc describe --namespace=openshift-ingress-operator ingresscontroller/default | grep "Domain:" | cut -d ":" -f 2 | cut -d "." -f 1-)
+export defaultdomain=$(oc describe --namespace=openshift-ingress-operator ingresscontroller/default | grep "Domain:" | cut -d ":" -f 2 | cut -d "." -f 1- | tr -d ' ')
 oc expose svc/winmachineman --hostname=winmachineman.$defaultdomain
 sleep 20
 #oc set env dc/winmachineman  MYURL=https://media.githubusercontent.com/media/glennswest/wcontent/master
